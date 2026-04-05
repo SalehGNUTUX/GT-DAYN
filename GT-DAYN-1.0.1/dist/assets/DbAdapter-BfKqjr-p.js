cbf6f7029068b228af3318d9473aa87999a98fce
GNUTUX <salehoukiki@gmail.com>
2026-04-05 18:56:23 +0100

    Add files via upload

---
+++ GT-DAYN-1.0.1/dist/assets/DbAdapter-BfKqjr-p.js
@@ -0,0 +1,45 @@
+class DbAdapter {
+  /** تهيئة الاتصال وتشغيل schema.sql */
+  async init() {
+    throw new Error("Not implemented");
+  }
+  /**
+   * تنفيذ استعلام يُعيد صفوفاً
+   * @param {string} sql
+   * @param {Array}  params
+   * @returns {Promise<Array<Object>>}
+   */
+  async query(sql, params = []) {
+    throw new Error("Not implemented");
+  }
+  /**
+   * تنفيذ استعلام تعديل (INSERT / UPDATE / DELETE)
+   * @param {string} sql
+   * @param {Array}  params
+   * @returns {Promise<{ lastInsertRowid: number, changes: number }>}
+   */
+  async run(sql, params = []) {
+    throw new Error("Not implemented");
+  }
+  /**
+   * تصدير قاعدة البيانات كـ Uint8Array (للنسخ الاحتياطي و Drive)
+   * @returns {Promise<Uint8Array>}
+   */
+  async export() {
+    throw new Error("Not implemented");
+  }
+  /**
+   * استيراد قاعدة بيانات من Uint8Array
+   * @param {Uint8Array} data
+   */
+  async import(data) {
+    throw new Error("Not implemented");
+  }
+  /** إغلاق الاتصال */
+  async close() {
+    throw new Error("Not implemented");
+  }
+}
+export {
+  DbAdapter as D
+};
